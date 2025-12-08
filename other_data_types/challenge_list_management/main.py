1d9539c12baddf208764a988fff3bdc3a7dd1f80
csalvador-bsa <>
2025-12-08 23:43:38 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,19 @@
+# Define individual grocery items as lists containing details
+meat = ["Ham", 3.99, 50, "Sliced"] # Item name, price, quantity, type
+cheese = ["Cheddar", 5.49, 100, "Sharp"]   # Item name, price, quantity, type
+condiment = ["Mustard", 1.99, 75, "Spicy"] # Item name, price, quantity, type
+
+# Create the main grocery list that contains these items
+deli_dept = [meat, cheese, condiment]
+print("Grocery List:" , grocery_list)
+
+# Restock Item
+If Ham in meat and quantity 
+
+
+
+
+print("Item:", grocery_list[2][0]) # Accesses "Milk" title
+print("Price:", grocery_list[2][1]) # Accesses price of a Milk, which is 5.99
+print("Quantity:", grocery_list[2][2]) # Accesses quantity of Milk, which is 2
+print("Type:", grocery_list[2][3]) # Accesses type of Milk, which is "2% Milk"
